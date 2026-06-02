ece2ba143006e967add9d391a3c308e985cf5ecd
Ambati Navaneeth <ambatinavaneeth9@gmail.com>
2026-06-02 17:08:25 +0530

    Implement array rotation functions
    Added functions for right and left rotation of an array.

--- programs.py
+++ programs.py
@@ -2,3 +2,27 @@ move zeros
 maj element
 nearest prime
 all permutations of strimg/list
+
+right rotation end to start
+d=4
+arr=[1,2,3,4,5]
+d %= len(arr)
+arr.reverse()
+arr[:d] = reversed(arr[:d])
+arr[d:] = reversed(arr[d:])
+print(arr)
+
+# Right rotation
+arr[-d:] + arr[:-d]
+
+# Left rotation
+arr[d:] + arr[:d]
+
+left rotation start to end 
+d=4
+arr=[1,2,3,4,5]
+d %= len(arr)
+arr[:d] = reversed(arr[:d])
+arr[d:] = reversed(arr[d:])
+arr.reverse()
+print(arr)
